453392af4854e1b0b67387e2a163420968bb53d5
rhit-fengr <fengr@rose-hulman.edu>
2022-01-14 13:27:10 -0500

    project and drawer set up
    working on getting both drawer and bot nav.

---
+++ main/RoseRide/app/src/main/java/edu/rosehulman/roseride/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,41 @@
+package edu.rosehulman.roseride.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import edu.rosehulman.roseride.R
+import edu.rosehulman.roseride.databinding.FragmentGalleryBinding
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+    private var _binding: FragmentGalleryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        galleryViewModel =
+            ViewModelProvider(this).get(GalleryViewModel::class.java)
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ main/RoseRide/app/src/main/java/edu/rosehulman/roseride/ui/user/UserFragment.kt
@@ -0,0 +1,39 @@
+package edu.rosehulman.roseride.ui.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import coil.load
+import coil.transform.CircleCropTransformation
+import edu.rosehulman.roseride.databinding.FragmentUserBinding
+
+
+class UserFragment : Fragment() {
+
+    private lateinit var binding: FragmentUserBinding
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentUserBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        updateView()
+        return root
+    }
+
+    private fun updateView() {
+        binding.centerImage.load("https://media-exp1.licdn.com/dms/image/C4E03AQHYevMgxmPPMw/profile-displayphoto-shrink_200_200/0/1603517211239?e=1647475200&v=beta&t=LZFiFKxY_7fBwUkRMAqvqPRBtWCP5FKc4qT6OtoqUl0"){
+            crossfade(true)
+            transformations(CircleCropTransformation())
+        }
+    }
+
+}
